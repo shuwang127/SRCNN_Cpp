026e7e6a590632ccad34c523a9115f2af9db2c1e
Shu Wang <shuwang127@yahoo.com>
2015-12-31 22:33:58 +0800

    Update srcnn.sh

--- srcnn.sh
+++ srcnn.sh
@@ -15,4 +15,19 @@ then
 fi
 
 cd ../
-./opencv.sh SRCNN $1
+
+tStart=$(date +%s)
+./SRCNN $1
+tEnd=$(date +%s)
+
+echo -e "  \033[0;36;1m[Info]\033[0m The program take \033[0;31;1m$((tEnd-tStart))\033[0m seconds ..."
+
+if [ $? -eq 0 ];
+then
+	echo -e "  \033[0;32;1m[Running]\033[0m Complete -- Success!"
+else
+	echo -e "  \033[0;31;1m[Error]\033[0m Running Failed!"
+	exit 6
+fi
+
+exit 0
